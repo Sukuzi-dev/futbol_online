7386673133a494d18fe6a8c6abb0d4d938e621ab
Sukuzi-dev <jimenezcardozojean@gmail.com>
2026-06-30 19:46:48 +0200

    r2.js

--- config/r2.js
+++ config/r2.js
@@ -1,4 +1,6 @@
 const { S3Client } = require('@aws-sdk/client-s3');
+const https = require('https');
+const http = require('http');
 
 const r2Client = new S3Client({
     region: 'auto',
@@ -6,6 +8,13 @@ const r2Client = new S3Client({
     credentials: {
         accessKeyId: process.env.R2_ACCESS_KEY_ID,
         secretAccessKey: process.env.R2_SECRET_ACCESS_KEY
+    },
+    forcePathStyle: true,
+    requestHandler: {
+        httpsAgent: new https.Agent({
+            rejectUnauthorized: false,
+            keepAlive: true
+        })
     }
 });
 
