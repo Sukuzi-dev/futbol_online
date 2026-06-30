32c69c28ab30b9a718595cfeae73b493c01ed62d
Sukuzi-dev <jimenezcardozojean@gmail.com>
2026-06-30 18:14:00 +0200

    server.js
    Removed MySQL connection setup and dotenv configuration.

--- server.js
+++ server.js
@@ -17,25 +17,6 @@ const io = socketIO(server, {
     pingInterval: 25000
 });
 
-const mysql = require('mysql2/promise');
-require('dotenv').config();
-
-if (process.env.DATABASE_URL) {
-    // Railway proporciona DATABASE_URL
-    const url = new URL(process.env.DATABASE_URL);
-    pool = mysql.createPool({
-        host: url.hostname,
-        port: url.port,
-        user: url.username,
-        password: url.password,
-        database: url.pathname.replace('/', ''),
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0,
-        charset: 'utf8mb4'
-    });
-}
-
 // Seguridad
 app.use(helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false }));
 app.use(cors());
